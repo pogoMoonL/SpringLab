48077e757b544ab15209e3e07b40dfddc0549f18
yaodehaung <sef1548@gmail.com>
2021-01-22 21:21:20 +0800

    fix: add mutiple find

--- src/main/java/com/nickhuang/springLab/App.java
+++ src/main/java/com/nickhuang/springLab/App.java
@@ -7,9 +7,12 @@ import org.springframework.boot.SpringApplication;
  * Hello world!
  *
  */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-public class App
-{
+public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
--- src/main/java/com/nickhuang/springLab/controller/GreetingController.java
+++ src/main/java/com/nickhuang/springLab/controller/GreetingController.java
@@ -2,18 +2,55 @@ package com.nickhuang.springLab.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.nickhuang.springLab.model.Greeting;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nickhuang.springLab.model.Tutorial;
+import com.nickhuang.springLab.repository.TutorialRepository;
+
 @RestController
+@RequestMapping("/api")
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+    @Autowired
+    TutorialRepository tutorialRepository;
+
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+            if (title == null)
+                tutorialRepository.findAll().forEach(tutorials::add);
+            else
+                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
